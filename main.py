092f5534e3454468595405c100cec6885e400c54
Gregory Ditzler <gregory.ditzler@gmail.com>
2023-01-27 16:49:00 -0500

    moved the helper functions from main.py to utils.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ import pickle
 import numpy as np 
 from muscle.data import DataLoader
 from muscle.models import DenseNet121
-
+from muscle.utils import load_train_evaluate
 
 path_adversarial_data = 'outputs/'
 path_output = 'outputs/multi_resultion_performances.pkl'
@@ -35,60 +35,7 @@ params = {
     'augment': False, 
     'store_numpy': True 
 }
-epsilons = [(i+1)/100 for i in range(20)]
-
-def prepare_adversarial_data(file_path, image_size): 
-    data_dict = pickle.load(open(file_path, 'rb'))
     
-    # resize the images to accomodate for the model's expected shape. 
-    if data_dict['X_adv'].shape[1] != image_size:
-        Xadv = np.zeros((data_dict['Xadv'].shape[0], image_size, image_size, 3))
-        for i in range(len(Xadv)): 
-            Xadv[i] = cv2.resize(data_dict['Xadv'], (image_size, image_size))
-        yadv = data_dict['y']
-    else: 
-        Xadv, yadv = data_dict['X_adv'], data_dict['y']  
-    
-    return Xadv, yadv
-     
-def load_train_evaluate(image_size): 
-    dataloader = DataLoader(
-        image_size=image_size, 
-        batch_size=params['batch_size'], 
-        rotation=params['rotation'], 
-        augment=params['augment'], 
-        store_numpy=True
-    )
-       
-    network = DenseNet121(
-        learning_rate=0.0005, 
-        image_size=80, 
-        epochs=10
-    )
-    network.train(dataloader)
-    
-    performance = {}
-    performance['Benign'] = network.network.evaluate(dataloader.X_valid, dataloader.y_valid)
-    
-    perf = np.zeros((len(epsilons,)))
-    for n, eps in enumerate(epsilons): 
-        file_path = ''.join(path_adversarial_data, 'Adversarial_FastGradientMethod_eps', str(eps), '.pkl')
-        Xadv, yadv = prepare_adversarial_data(file_path=file_path, image_size=image_size)
-        perf[n] = network.network.evaluate(Xadv, yadv)
-    performance['FastGradientMethod'] = perf
-   
-    perf = np.zeros((len(epsilons,)))
-    for n, eps in enumerate(epsilons): 
-        file_path = ''.join(path_adversarial_data, 'Adversarial_ProjectedGradientDescent_eps', str(eps), '.pkl')
-        Xadv, yadv = prepare_adversarial_data(file_path=file_path, image_size=image_size)
-        perf[n] = network.network.evaluate(Xadv, yadv)
-    performance['FastGradientMethod'] = perf 
-    
-    file_path = ''.join(path_adversarial_data, 'Adversarial_DeepFool.pkl')
-    Xadv, yadv = prepare_adversarial_data(file_path=file_path, image_size=image_size)
-    performance['DeepFool'] = network.network.evaluate(Xadv, yadv)
-    return performance
-
 def main():
     performance_160 = load_train_evaluate(160)
     performance_80 = load_train_evaluate(80)
@@ -102,7 +49,6 @@ def main():
     with open(path_output, 'rb') as f:
         pickle.dump(results, f) 
         
-    
 
 if __name__ == '__main__': 
     main()
--- muscle/utils.py
+++ muscle/utils.py
@@ -24,3 +24,60 @@ import pickle
 import numpy as np 
 import tensorflow as tf
 import tensorflow_datasets as tfds
+from .models import DenseNet121
+
+epsilons = [(i+1)/100 for i in range(20)]
+ 
+def prepare_adversarial_data(file_path, image_size): 
+    data_dict = pickle.load(open(file_path, 'rb'))
+    
+    # resize the images to accomodate for the model's expected shape. 
+    if data_dict['X_adv'].shape[1] != image_size:
+        Xadv = np.zeros((data_dict['Xadv'].shape[0], image_size, image_size, 3))
+        for i in range(len(Xadv)): 
+            Xadv[i] = cv2.resize(data_dict['Xadv'], (image_size, image_size))
+        yadv = data_dict['y']
+    else: 
+        Xadv, yadv = data_dict['X_adv'], data_dict['y']  
+    
+    return Xadv, yadv
+ 
+
+def load_train_evaluate(image_size): 
+    dataloader = DataLoader(
+        image_size=image_size, 
+        batch_size=params['batch_size'], 
+        rotation=params['rotation'], 
+        augment=params['augment'], 
+        store_numpy=True
+    )
+       
+    network = DenseNet121(
+        learning_rate=0.0005, 
+        image_size=80, 
+        epochs=10
+    )
+    network.train(dataloader)
+    
+    performance = {}
+    performance['Benign'] = network.network.evaluate(dataloader.X_valid, dataloader.y_valid)
+    
+    perf = np.zeros((len(epsilons,)))
+    for n, eps in enumerate(epsilons): 
+        file_path = ''.join('outputs/Adversarial_FastGradientMethod_eps', str(eps), '.pkl')
+        Xadv, yadv = prepare_adversarial_data(file_path=file_path, image_size=image_size)
+        perf[n] = network.network.evaluate(Xadv, yadv)
+    performance['FastGradientMethod'] = perf
+   
+    perf = np.zeros((len(epsilons,)))
+    for n, eps in enumerate(epsilons): 
+        file_path = ''.join('outputs/Adversarial_ProjectedGradientDescent_eps', str(eps), '.pkl')
+        Xadv, yadv = prepare_adversarial_data(file_path=file_path, image_size=image_size)
+        perf[n] = network.network.evaluate(Xadv, yadv)
+    performance['FastGradientMethod'] = perf 
+    
+    file_path = ''.join('outputs/Adversarial_DeepFool.pkl')
+    Xadv, yadv = prepare_adversarial_data(file_path=file_path, image_size=image_size)
+    performance['DeepFool'] = network.network.evaluate(Xadv, yadv)
+    return performance
+
