b06f083780a416fd760682721cce606ecc6f582b
Gregory Ditzler <gregory.ditzler@gmail.com>
2023-02-26 09:34:06 -0500

    added fisher information loss

--- muscle/models.py
+++ muscle/models.py
@@ -33,7 +33,23 @@ def get_backbone(backbone:str='DenseNet121') :
         model_backbone = tf.keras.applications.vgg19.VGG19
     else: 
         raise(ValueError(''.join(['Uknown backbone: ', backbone])))
-    return model_backbone 
+    return model_backbone
+
+class FisherInformationLoss(tf.keras.losses.Loss): 
+
+    def __init__(self, lambda_reg:float=0.5, soften:bool=False): 
+        super().__init__()
+        self.lambda_reg = lambda_reg
+        self.soften = soften
+        self.epsilon = 1e-6 
+    
+    def call(self, y_true, y_pred):
+        cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_true * tf.log(y_pred), reduction_indices=[1]))
+        if self.soften: 
+            fisher = tf.reduce_mean(tf.log(1.0/(y_pred+self.epsilon)))
+        else: 
+            fisher = tf.reduce_mean(1.0/(y_pred+self.epsilon)) 
+        return cross_entropy + self.lambda_reg*fisher
 
 class VanillaCNN:
     """
