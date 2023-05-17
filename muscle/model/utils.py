fb138ae3003ba7fdf83a225597f349c4b23358e6
Gregory Ditzler <gregory.ditzler@gmail.com>
2023-05-17 12:10:59 -0400

    added doc strings

--- muscle/model/utils.py
+++ muscle/model/utils.py
@@ -24,8 +24,15 @@ import numpy as np
 import tensorflow as tf 
 
 def get_backbone(backbone:str='DenseNet121'):
+    """Fetch the pretrained neural network. 
+
+    Args:
+        backbone (str, optional): Neural network backbone from tensorgflow.keras.applications. 
+           The available backbones are VGG19 and DenseNet121. Defaults to 'DenseNet121'. 
+
+    Returns:
+        tensorflow model: pretrained backbone without the head of the network. 
     """
-    """ 
     if backbone == 'DenseNet121': 
         model_backbone = tf.keras.applications.densenet.DenseNet121 
     elif backbone == 'ResNet50': 
@@ -59,7 +66,17 @@ class FisherInformationLoss(tf.keras.losses.Loss):
         return cross_entropy + self.lambda_reg*fisher
 
 
-def read_score(model, X:np.ndarray=None, Xa:np.ndarray=None):
+def read_score(model, X:np.ndarray, Xa:np.ndarray):
+    """_summary_
+
+    Args:
+        model (tensorflow.model): Tensorflow neural network (trained)
+        X (np.ndarray): Benign data. 
+        Xa (np.ndarray): Adversarial data.
+
+    Returns:
+        _type_: _description_
+    """
     p_clean = model.predict(X) 
     p_adversary = model.predict(Xa) 
     score = 0. 
@@ -69,6 +86,16 @@ def read_score(model, X:np.ndarray=None, Xa:np.ndarray=None):
     return score
 
 def read_score_mrn(model, dataset_benign, dataset_adversary): 
+    """_summary_
+
+    Args:
+        model (tensorflow.model): Tensorflow neural network. 
+        dataset_benign (muscle.data.FusionDataLoader): Dataloader with benign data 
+        dataset_adversary (muscle.data.FusionDataLoader): Dataloader with adversarial data 
+
+    Returns:
+        _type_: _description_
+    """
     data_benign = (dataset_benign.X1, dataset_benign.X2, dataset_benign.X3)
     p_clean = model.predict(data_benign)
     data_adversary= (dataset_adversary.X1, dataset_adversary.X2, dataset_adversary.X3)
